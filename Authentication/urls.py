c8ac4afb5e90f3b79e958baddb449627da11bdf5
Ritik <ritiksapat@gmail.com>
2023-07-05 22:19:06 +0530

    signin signout

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = 'auth'
 
 urlpatterns = [
-   path('sign_up/',views.sign_up,name='signup'),
+    path('sign_up/', views.sign_up, name='signup'),
+    path('sign_in/', views.sign_in, name='signin'),
+    path('logout/', views.logout_user, name='logout'),
 
 ]
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -19,3 +23,30 @@ def sign_up(request):
         'is_valid':is_valid
 
     })
+
+
+def sign_in(request):
+    form = AuthenticationForm()
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+    
+    return render(request,'Authentication/login.html',context={
+        'form':form,
+    })
+
+
+@login_required()
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
